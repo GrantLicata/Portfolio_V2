74e8ed4a43319f22b9148b0fe7d31367e8642a89
GrantLicata <grant.licata@gmail.com>
2024-07-10 19:37:13 -0700

    Description Added for title

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,10 +1,20 @@
 const Title = () => {
   return (
-    <div className="text-white pb-20 pl-10">
-      <h3>Hi, my name is</h3>
-      <h1 className="text-3xl">Grant Licata.</h1>
-      <h1 className="text-3xl">I lead projects to successful</h1>
-      <h1 className="text-3xl">outcomes.</h1>
+    <div className="pl-10 max-w-[600px]">
+      <div className="text-white pb-6">
+        <h3 className="pb-4">Hi, my name is</h3>
+        <h1 className="text-3xl">Grant Licata.</h1>
+        <h1 className="text-3xl">I lead projects to successful</h1>
+        <h1 className="text-3xl">outcomes.</h1>
+      </div>
+      <div>
+        <h3 className="text-white">
+          I'm a project manager specializing in the leadership of
+          cross-functional teams within the pharmaceuticals and biotechnology
+          space. Currently, I'm leading teams in the development and manufacture
+          of biologics.
+        </h3>
+      </div>
     </div>
   );
 };
